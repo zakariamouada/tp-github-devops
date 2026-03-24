83fd52dc0086feb5b078993e66743b068bf9e450
zakariamouada <zakaria.mouada@etu.uae.ac.ma>
2026-03-24 15:52:25 +0100

    feat: add saluer and calculer_imc functions

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Hello GitHub!")
+def saluer(nom):
+    return f"Bonjour depuis GitHub, {nom} !"
+
+def calculer_imc(poids, taille):
+    return round(poids / (taille ** 2), 2)
+
+if __name__ == "__main__":
+    print(saluer("DevOps"))
+    print("IMC:", calculer_imc(70, 1.75))
