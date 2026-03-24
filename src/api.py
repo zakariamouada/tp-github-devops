ae7b400f8267cab2e99736038ca005e4db5b9397
zakariamouada <zakaria.mouada@etu.uae.ac.ma>
2026-03-24 16:36:28 +0100

    WIP: list function

--- src/api.py
+++ src/api.py
@@ -4,3 +4,4 @@ def ajouter_utilisateur(nom, email):
     utilisateurs.append({"nom": nom, "email": email})
     return utilisateurs
 # Mise à jour API
+def lister_utilisateurs(): return utilisateurs
