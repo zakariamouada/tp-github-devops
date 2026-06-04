0350351f3c252dd9d1a9d068196673fd1b30b499
zakariaMD24 <koninsan04@gmail.com>
2026-06-04 15:16:55 +0100

    feat: add saluer and calculer_imc functions

--- app.py
+++ app.py
@@ -7,6 +7,3 @@ def calculer_imc(poids, taille):
 if __name__ == "__main__":
     print(saluer("DevOps"))
     print("IMC:", calculer_imc(70, 1.75))
-
-def formater_message(msg, majuscules=False):
-    return msg.upper() if majuscules else msg.lower()
